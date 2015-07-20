3278ae91f5aea3d432b5f55961ae8f6ab6c9cdb6
Jeffry Turcios <jefoutta@gmail.com>
2015-07-19 22:25:10 -0600

    Review fixes and recomendations

--- app/src/main/java/gt/tx/jefsportafolio/HomeActivity.java
+++ app/src/main/java/gt/tx/jefsportafolio/HomeActivity.java
@@ -1,17 +1,18 @@
 package gt.tx.jefsportafolio;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-    public Toolbar mActionBarToolbar;
+    protected Toolbar mActionBarToolbar;
+    protected Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +21,16 @@ public class HomeActivity extends AppCompatActivity {
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
         if (mActionBarToolbar != null) {
             setSupportActionBar(mActionBarToolbar);
-//            getSupportActionBar().setHomeButtonEnabled(true);
-//            getSupportActionBar().setHomeButtonEnabled(false);
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }else{
         }
+
+        final Resources resources = getResources();
+        mToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
 
         Button firstAppBtn = (Button) findViewById(R.id.firstAppBtn);
         firstAppBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                printMessage("Launch SPOTIFY STREAMER APP");
+                printMessage(resources.getString(R.string.launch, resources.getString(R.string.first_app_name)));
             }
         });
 
@@ -38,7 +38,7 @@ public class HomeActivity extends AppCompatActivity {
         secondAppBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                printMessage("Launch SCORES APP");
+                printMessage(resources.getString(R.string.launch, resources.getString(R.string.second_app_name)));
             }
         });
 
@@ -46,7 +46,7 @@ public class HomeActivity extends AppCompatActivity {
         thirdAppBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                printMessage("Launch LIBRARY APP");
+                printMessage(resources.getString(R.string.launch, resources.getString(R.string.third_app_name)));
             }
         });
 
@@ -54,7 +54,7 @@ public class HomeActivity extends AppCompatActivity {
         fourthAppBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                printMessage("Launch BUILD IT BETTER APP");
+                printMessage(resources.getString(R.string.launch, resources.getString(R.string.fourth_app_name)));
             }
         });
 
@@ -62,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
         fifthAppBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                printMessage("Launch XYZ READER APP");
+                printMessage(resources.getString(R.string.launch, resources.getString(R.string.fifth_app_name)));
             }
         });
 
@@ -70,34 +70,23 @@ public class HomeActivity extends AppCompatActivity {
         sixthAppBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                printMessage("Launch CAPSTONE: MY OWN APP");
+                printMessage(resources.getString(R.string.launch, resources.getString(R.string.sixth_app_name)));
             }
         });
     }
 
     protected void printMessage(String s){
-        Toast.makeText(getBaseContext(), s, Toast.LENGTH_SHORT).show();
+        mToast.cancel();
+//        mToast.setText("Veamos");
+//        mToast.setDuration(Toast.LENGTH_SHORT);
+        mToast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        mToast.show();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+//        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return false;
     }
 }
